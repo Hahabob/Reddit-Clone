8eb492d50fedc2b06f895360dcd1fa87d257cf07
hahabob <orisor101@gmail.com>
2025-08-31 20:23:53 +0300

    added comments

--- server/src/routes/comment.ts
+++ server/src/routes/comment.ts
@@ -1,17 +1,17 @@
 import experss from "express";
 
 const router = experss.Router();
-
+//Get all comments for a post
 router.get("/posts/:postId/comments");
-
+//Create new comment
 router.post("/posts/:postId/comments");
-
+//Get nested replies for a comment
 router.get("/comments/:id/replies");
-
+//Edit comment
 router.put("/comments/:id");
-
+//Delete comment
 router.delete("/comments/:id");
-
+//Upvote comment
 router.post("/comments/:id/upvote");
-
+//Downvote comment
 router.post("/comments/:id/downvote");
