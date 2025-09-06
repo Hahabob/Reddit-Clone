484f7f2e2dfff0a28df587d80a046e12391cca06
omerlen312 <omerlen1000@gmail.com>
2025-09-06 15:57:54 +0300

    made sign in and sign up pages

--- Client/src/components/Layout/MainLayout.tsx
+++ Client/src/components/Layout/MainLayout.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useRef } from "react";
+import { SignedIn, SignedOut, UserButton } from "@clerk/clerk-react";
 import { Header } from "./Header";
 import { Sidebar } from "./Sidebar";
 import { RightSidebar } from "./RightSidebar";
@@ -44,9 +45,24 @@ export const MainLayout: React.FC = () => {
         <main className="flex-1 min-h-screen">
           <PostFeed ref={postFeedRef} />
         </main>
-        <aside className="hidden xl:block">
-          <RightSidebar />
-        </aside>
+        <div className="hidden xl:block">
+          <SignedOut>
+            <RightSidebar />
+          </SignedOut>
+          <SignedIn>
+            <div
+              className={`w-80 h-screen overflow-y-auto ${
+                isDarkMode ? "bg-gray-900" : "bg-white"
+              } border-l ${isDarkMode ? "border-gray-700" : "border-gray-200"}`}
+            >
+              <div className="p-4">
+                <div className="flex items-center justify-center">
+                  <UserButton afterSignOutUrl="/" />
+                </div>
+              </div>
+            </div>
+          </SignedIn>
+        </div>
       </div>
     </div>
   );
