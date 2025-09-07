928f9564f70cc02a9c87a521ec7decb44c484dcb
hahabob <orisor101@gmail.com>
2025-09-07 15:49:28 +0300

    halfway done

--- server/src/routes/post.ts
+++ server/src/routes/post.ts
@@ -3,18 +3,18 @@ import PostController from "../controllers/PostController";
 
 const router = express.Router();
 //List all posts; supports query params: feed=home, communityId=<id>, `sort=new
-// router.get("/posts");
-// //Get single post with comments
-// router.get("/posts/:id");
+router.get("/", PostController.getAll);
+//Get single post with comments
+router.get("/:id", PostController.get);
 //Create new post
 router.post("/", PostController.create);
 //Edit post
-// router.put("/posts/:id");
-// //Delete post
-// router.delete("/posts/:id");
-// //Upvote post
-// router.post("/posts/:id/upvote");
-// //Downvote post
-// router.post("/posts/:id/downvote");
+// router.patch("/:id"); //todo implement
+//Delete post
+router.delete("/:id", PostController.delete);
+//Upvote post
+// router.post("/:id/upvote");
+//Downvote post
+// router.post("/:id/downvote");
 
 export default router;
