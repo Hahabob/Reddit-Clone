414f6398780b1f5bcb7c5cc5a6ac9c4dc252791b
hahabob <orisor101@gmail.com>
2025-09-09 00:58:43 +0300

    most basic functions exist

--- server/src/routes/post.ts
+++ server/src/routes/post.ts
@@ -8,9 +8,16 @@ router.get("/", PostController.getAll);
 router.get("/:id", PostController.get);
 //Create new post
 router.post("/", PostController.create);
-//Edit post
-// router.patch("/:id"); //todo implement
-//Delete post
+//Edit post only content same as reddit
+router.patch("/:id", PostController.edit);
+//Delete post only for development, in reddit deleting a post is adding a delete flag and hiding it from the feed
 router.delete("/:id", PostController.delete);
-
+//Tag post as nsfw
+router.patch("/:id/nsfw", PostController.tagNsfwPost);
+//Tag post as spoiler
+router.patch("/:id/spoiler", PostController.tagSpoilerPost);
+//Pin post
+// router.patch("/:id/pin", PostController.pinPost);
+//Archive post
+// router.patch("/:id/archive", PostController.archivePost);
 export default router;
