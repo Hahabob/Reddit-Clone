a7bcb6787d63b9479ddac426c0d79fb78d49e516
omerlen312 <omerlen1000@gmail.com>
2025-09-28 08:50:43 +0300

    feat: Add comment count functionality to PostCard component

--- Client/src/hooks/useComments.ts
+++ Client/src/hooks/useComments.ts
@@ -265,3 +265,8 @@ export const useRemoveComment = () => {
     },
   });
 };
+
+export const usePostCommentCount = (postId: string) => {
+  const { data: comments } = useComments(postId);
+  return comments ? comments.length : 0;
+};
